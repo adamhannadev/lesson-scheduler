ffea6b0636ed98f9c4c6ba2490308b30542c9636
adamhannadev <adamhannadev@gmail.com>
2022-02-02 19:53:56 +0000

    Add before_create callback to model to change the default end time

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+    before_create do
+        self.event_end = self.event_start + 1.hour
+    end
+
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -4,4 +4,11 @@ class EventTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "event_end saved as 1 hour later by default" do
+    e = Event.new()
+    e.event_start = Time.zone.now
+    e.save()
+
+    assert_equal e.event_end, e.event_start + 1.hour, "End time is not 1 hour after start time"
+  end
 end
