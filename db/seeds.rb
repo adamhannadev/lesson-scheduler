090cd38a78660b59281e8181a4bbed09af616459
adamhannadev <adamhannadev@gmail.com>
2022-02-06 23:20:33 +0000

    Add db seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Student.create([
+    {last_name: "Norman", first_name: "Kami", phone: "250-846-6659", email: "askljcalb@sclasc.com"},
+    {last_name: "Gould", first_name: "Linda", phone: "778-542-5129", email: "asclksn@acklnsa.com"}
+])
+Teacher.create([
+    {last_name: "Hanna", first_name: "Adam", phone: "250-480-9246", email: "info@adamhannaballroom.com"},
+    {last_name: "Kottova", first_name: "Tyna", phone: "778-542-5129", email: "tyna@adamhannaballroom.com"}
+])
+Lesson.create([
+    {
+        start_time: Time.zone.now, end_time: Time.zone.now + 1.hour, student: Student.first, teacher: Teacher.first
+    }
+])
