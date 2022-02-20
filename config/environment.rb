cf1ea4dd44ada2eac4a8bb68b190e7725ed299ea
adamhannadev <adamhannadev@gmail.com>
2022-02-20 01:38:38 +0000

    Configure action mailer for production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.raise_delivery_errors = true
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+:address   => "mail.adamhannaballroom.com",
+:domain => 'ahbd-scheduler.herokuapp.com',
+:port      => 465,
+:user_name => "mailer@adamhannaballroom.com",
+:password  => ENV['SMTP_PASSWORD'], 
+:authentication => :plain,
+:enable_starttls_auto => true
+}
