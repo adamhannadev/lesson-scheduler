a3cd8d77497a03885cc93d558ccfa3aa8fd2ebb8
adamhannadev <adamhannadev@gmail.com>
2022-03-06 00:55:37 +0000

    Correct duration helper in lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,7 +8,12 @@ class Lesson < ApplicationRecord
   before_save :create_default_ending
 
   def duration
-    (self.end_time - self.start_time).minutes
+    difference = ((self.end_time - self.start_time) / 1.hour).round(2).to_s
+    dur_array = difference.split(".").pop(2)
+    hours = dur_array[0].to_i
+    remainder = (dur_array[1].to_f / 100)
+    mins = (remainder * 60) / 100
+    return hours + mins.round(2)
    end
   private
 
